4e7e5c6a0cc34783be51d30df4de66a81d285ac0
Renuka <renukaboorsu@gmail.com>
2022-11-14 12:53:15 +0530

    Created sample code

---
+++ cal.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void cal(int a,int b)
+{
+	cout<<"Addition  is "<<a+b;
+        cout<<"\nsubtraction is "<<a-b;
+	cout<<"\nMultiplication is "<<a*b;
+	cout<<"\nDivision is "<<a/b;
+}
+int main()
+{
+	cal(5,3);
+	return 0;
+}
+
